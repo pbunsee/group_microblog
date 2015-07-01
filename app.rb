1633618f55ce0e00cd1cf93193ce2ae9370deaa7
Pranesha Bunsee <pbunsee@gmail.com>
2015-07-01 12:40:52 -0400

    Created sign-up page & routing

--- app.rb
+++ app.rb
@@ -4,3 +4,19 @@ require './models'
 
 set :database, "sqlite3:nottwitter.sqlite3"
 
+get '/sign-up' do
+  erb :sign_up
+end
+  
+
+post '/sign-up' do
+  confirmation = params[:confirm_password]
+  if confirmation = params[:user][:password]
+    @user = User.create(params[:user])
+    "SIGNED UP #{@user.username}"
+  else
+    "Your password & confirmation password did not match. Try again."
+  end
+end
+
+
