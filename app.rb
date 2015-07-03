da26e50718d2766fbfe644ad4d8522a69c992541
Pranesha Bunsee <pbunsee@gmail.com>
2015-07-03 18:22:37 -0400

    Styled login page. Implemented "Delete Profile" Added user_id into post insert call Fixed posts retrieval to use the object instance instead of class method Added condition on the post.erb form to check if @posts has more than zero values.

--- app.rb
+++ app.rb
@@ -11,33 +11,51 @@ use Rack::Flash, sweep: true
 get "/" do
   if current_user
     @current_user = current_user
-    erb :index
+    redirect '/post'
   else
-    puts "redirect to sign-in 1"
     redirect '/sign-in'
   end
 end
 
 get '/post' do
-  @stylesheet = 'styles/post.css'
-  erb :post
+  @post = {}
+  if current_user
+    user_id = session[:user_id]
+    @post = User.find(user_id).posts
+    puts "user_id #{user_id} has #{@post.size} posts"
+    @stylesheet = 'styles/post.css'
+    erb :post
+  else
+    redirect '/sign-in'
+  end
 end
 
 post '/post' do
-  @post = Post.create({body: params[:post]})
-  @stylesheet = 'styles/post.css'
-  erb :post
+  if current_user
+    @post = Post.create({body: params[:post], user_id: session[:user_id]})
+    # or could do @post = User.find(user_id).post.create(:post)
+    @stylesheet = 'styles/post.css'
+    erb :post
+  else
+    redirect '/sign-in'
+  end
 end
 
 get '/sign-out' do
-  user_id = session[:user_id]
-  @user = User.find(user_id)
-  session.clear
-  flash[:notice] = "Signed Out Successfully.  Come back soon!"
-  redirect '/'
+  if current_user
+    user_id = current_user
+    @user = User.find(user_id)
+    session[:user_id] = nil;   
+    session.clear
+    flash[:notice] = "Signed Out Successfully.  Come back soon!"
+    redirect '/'
+  else
+    redirect '/sign-in'
+  end
 end
 
 get '/sign-up' do 
+    session[:user_id] = nil;   
   erb :sign_up 
 end
 
@@ -47,8 +65,8 @@ post '/sign-up' do
     @user = User.create(params[:user])
     params[:profile].merge!(user_id: @user.id)
     Profile.create(params[:profile])
-    "SIGNED UP #{@user.username}"
-    erb :index
+    flash[:notice] = "Successfully signed up #{@user.username}!"
+    redirect '/sign-in'
   else
     flash[:notice] = "Please sign in"
     redirect '/'
@@ -56,6 +74,7 @@ post '/sign-up' do
 end
 
 get '/sign-in' do
+  @stylesheet = 'styles/sign_in.css'
   erb :sign_in
 end
 
@@ -66,7 +85,6 @@ post '/sign-in' do
   @user = User.where(username: username).first
 
   if @user == 'undefined' || @user.nil?
-    puts "@user is undefined, nil or empty for username: #{username} password: #{password}"
     flash[:notice] = "User not found. undefined, nil or empty for username: #{username} password: #{password}"
     redirect '/sign-up'
   else
@@ -75,26 +93,19 @@ post '/sign-in' do
       flash[:notice] = "Welcome #{@user.username}!"
       redirect '/post'
     else
-      puts "Password incorrect."
       flash[:notice] = "Incorrect username or password. Please try again."
       redirect '/sign-in'
     end
-    puts "username: #{username}"
-    puts "password: #{password}"
-    puts "@user: #{@user}"
-    puts "@user.username: #{@user.username}"
-    puts "@user.password: #{@user.password}"
   end
 end
  
 get '/edit-profile' do
   if current_user
-    user_id = session[:user_id]
+    user_id = current_user
     @user = User.find(user_id)
     @profile = Profile.where(user_id: user_id).first
     erb :edit_profile
   else
-    puts "redirect to sign-in 2"
     redirect '/sign-in'
   end
 end
@@ -103,28 +114,24 @@ post '/edit-profile' do
   if current_user
     erb :edit_profile
   else
-    puts "redirect to sign-in 3"
     redirect '/sign-in'
   end
 end
 
 get '/view-profile' do
-  puts "session user_id #{session[:user_id]}"
   puts current_user
   if current_user
-    user_id = session[:user_id]
+    user_id = current_user
     @user = User.find(user_id)
     @profile = Profile.where(user_id: user_id).first
+    # or could use: @profile = User.find(user_id).profile
     if @user.nil? || @user == 'undefined'
       puts "cannot find the profile for #{user_id}"
       flash[:notice] = "Profile not found."
     else
-      puts "view profile of user:  #{@profile}"
-      puts "this is the userID : #{user_id} for #{@user}"
       erb :view_profile
     end
   else
-    puts "redirect to sign-in 4"
     redirect '/sign-in'
   end
 end
@@ -133,7 +140,6 @@ post '/view-profile' do
   if current_user
     erb :view_profile
   else
-    puts "redirect to sign-in 5"
     redirect '/sign-in'
   end
 end
@@ -142,23 +148,25 @@ get '/delete-profile' do
   if current_user
     erb :delete_profile
   else
-    puts "redirect to sign-in 6"
     redirect '/sign-in'
   end
 end
 
 post '/delete-profile' do
   if current_user
-    erb :delete_profile
+    user_id = current_user
+    User.find(user_id).posts.destroy
+    User.find(user_id).profile.destroy
+    User.find(user_id).destroy
+    session.clear
   else
-    puts "redirect to sign-in 7"
     redirect '/sign-in'
   end
 end
 
 def current_user
-    puts "session user_id #{session[:user_id]}"
   if session[:user_id]
+    puts "session user_id #{session[:user_id]}"
     User.find(session[:user_id])
   end
 end
